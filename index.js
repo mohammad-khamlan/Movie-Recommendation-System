91b9d7c7724dd45ecdcb6716d23c2fa5fc0dac47
Sami Imran <samiimran2016@gmail.com>
2020-12-19 02:13:05 +0200

    adding styles

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function bright() {
-    var first_word = document.getElementById('main-msg').innerText
+    var h1 = document.createElement("h1");
+
+    h1.setAttribute("style",
+        "font-family: 'sans - serif';\
+        color:#fffa76;\
+        float:left;");
+    h1.style.fontSize = '1.6rem';
+    h1.style.lineHeight = '1.3';
+
+    document.getElementById('main-msg1') = h1;
 }
