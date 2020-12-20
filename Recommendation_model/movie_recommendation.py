d63f324ca5bf99bb2e3d23a163e0eb6546c8e5d0
mohammad-khamlan <47076940+mohammad-khamlan@users.noreply.github.com>
2020-12-21 01:13:43 +0200

    Update movie_recommendation.py

--- Recommendation_model/movie_recommendation.py
+++ Recommendation_model/movie_recommendation.py
@@ -53,6 +53,6 @@ def get_user_id():
         return prediction
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 4999))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host = '0.0.0.0', debug=True, port = port)
 
