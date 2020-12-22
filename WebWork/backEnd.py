241e4bad69d03351f885af4ce843eca4d0728727
noha-isaed <thahernoha@gmail.com>
2020-12-22 11:37:01 +0200

    update in all files

--- WebWork/backEnd.py
+++ WebWork/backEnd.py
@@ -25,21 +25,39 @@ def Recommend_Movies():
         predict_movies_ids = json.dumps(predict_movies_ids)
         movies_titles = requests.post("https://data-filtering.herokuapp.com/titles", data = predict_movies_ids)
         print(type(movies_titles))
-        print(movies_titles["movies_titles"])
+
+        if 'json' in movies_titles.headers.get('Content-Type'):
+            js = movies_titles.json()
+        else:
+            print('Response content is not in JSON format.')
+            js = 'spam'
+        print(type(js))
+        #for item in js.itertools:
+            #print(item)
+        print(js["movies_titles"])
 
         return 'done'
     
-    """
+   
     elif request.method == "GET" :
 
         userId = request.arg.get('user')  
         predict_movies_ids = [414]#predict(userId)
         predict_movies_ids = json.dumps(predict_movies_ids)
         movies_titles = requests.post( url = "https://data-filtering.herokuapp.com/titles", data = predict_movies_ids)
-        #print(movies_titles)
-        #redirect(url_for('main', movies_titles = movies_titles))
+        
+        if 'json' in movies_titles.headers.get('Content-Type'):
+            js = movies_titles.json()
+        else:
+            print('Response content is not in JSON format.')
+            js = 'spam'
+        print(type(js))
+        #for item in js.itertools:
+            #print(item)
+        print(js["movies_titles"])
+
         return 'done'
-    """
+   
    
 if __name__ == '__main__':
 
